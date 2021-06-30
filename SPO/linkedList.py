449a45aa79baaaf1e9f94b5eb055033349c25520
nikitka9963 <86627898+nikitka9963@users.noreply.github.com>
2021-06-30 12:24:49 +0300

    Update linkedList.py

--- SPO/linkedList.py
+++ SPO/linkedList.py
@@ -2,59 +2,72 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def push(self, val):
+        if self.head is None:
+            self.head = Item(val)
+            return
+
+        lastItem = self.head
+
+        while lastItem.nextValue:
+            lastItem = lastItem.nextValue
+
+        lastItem.nextValue = Item(val)
+
+    def get(self, ind):
+        lastItem = self.head
+        boxIndex = 0
+
+        while boxIndex <= ind:
+            if boxIndex == ind:
+                return lastItem.cat
+
+            boxIndex += 1
+            lastItem = lastItem.nextValue
+
+    def remove(self, val):
+        global lastItem
+        headItem = self.head
+
+        if headItem is not None:
+            if headItem.value == val:
+                self.head = headItem.nextValue
+                return
+
+        while headItem is not None:
+            if headItem.value == val:
+                break
+
+            lastItem = headItem
+            headItem = headItem.nextValue
+
+        if headItem is None:
+            return
+
+        lastItem.nextValue = headItem.nextValue
+
     def __repr__(self):
         current = self.head
         str = '[ '
+
         while current is not None:
             str += f'{current.value},'
             current = current.nextValue
+
         str += ']'
+
         return str
 
     def contains(self, value):
-
         lastItem = self.head
+
         while lastItem:
             if value == lastItem.value:
                 return True
             else:
                 lastItem = lastItem.nextValue
+                
         return False
-
-    def push(self, newValue):
-        newItem = Item(newValue)
-        if self.head is None:
-            self.head = newItem
-            return
-        lastItem = self.head
-        while lastItem.nextValue:
-            lastItem = lastItem.nextValue
-        lastItem.nextValue = newItem
-
-    def get(self, itemIndex):
-        lastItem = self.head
-        boxIndex = 0
-        while boxIndex <= itemIndex:
-            if boxIndex == itemIndex:
-                return lastItem.cat
-            boxIndex = boxIndex + 1
-            lastItem = lastItem.nextValue
-
-    def remove(self, rmValue):
-        headItem = self.head
-
-        if headItem is not None:
-            if headItem.value == rmValue:
-                self.head = headItem.nextValue
-                return
-        while headItem is not None:
-            if headItem.value == rmValue:
-                break
-            lastItem = headItem
-            headItem = headItem.nextValue
-        if headItem is None:
-            return
-        lastItem.nextValue = headItem.nextValue
 
 
 class Item:
